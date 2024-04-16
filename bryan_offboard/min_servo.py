dd98fb6df7282f6ae38c1095ddb51fe1eedced8e
bryan <balfaro@princeton.edu>
2024-04-16 16:34:47 -0400

    including code for servos

---
+++ bryan_offboard/min_servo.py
@@ -0,0 +1,16 @@
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(26)
+
+try:
+	# while True:
+    servo.min()
+    # 	sleep(0.5)
+    # 	servo.mid()
+    # 	sleep(0.5)
+    #servo.max()
+    print('servo in min position')
+    #sleep(0.5)
+except KeyboardInterrupt:
+	print("Program stopped")
--- bryan_offboard/offboard_track_april.py
+++ bryan_offboard/offboard_track_april.py
@@ -10,7 +10,10 @@ import sys
 import numpy as np
 import cv2
 from pupil_apriltags import Detector, Detection
+import datetime
 
+
+''' need to rewrite for oak camera'''
 def get_depth_coords(x_rgb, y_rgb, rgb_width, rgb_height, depth_width, depth_height):
     RGB_FOV_HORIZONTAL = 69 # deg
     RGB_FOV_VERITCAL = 42
@@ -251,6 +254,125 @@ def main(args=None) -> None:
     print('Starting offboard control node...')
     rclpy.init(args=args)
     offboard_control = OffboardControl()
+
+
+    # Create pipeline
+    pipeline = dai.Pipeline()
+
+    # Define source and output
+    camRgb = pipeline.create(dai.node.ColorCamera)
+    xoutRgb = pipeline.create(dai.node.XLinkOut)
+    xoutRgb.setStreamName("rgb")
+    camRgb.setBoardSocket(dai.CameraBoardSocket.CAM_A)
+    #camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+
+    # Properties
+    # very slow camRgb.setPreviewSize(1920, 1080)
+    camRgb.setPreviewSize(400, 400)
+    camRgb.setInterleaved(False)
+    camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+
+    camRgb.preview.link(xoutRgb.input)
+
+    detector = Detector(
+            families="tag36h11",
+            nthreads=1,
+            quad_decimate=1.0,
+            quad_sigma=0.0,
+            refine_edges=1,
+            decode_sharpening=0.25,
+            debug=0
+        )
+
+    # cx = 331.85198975
+    # cy = 247.21032715
+    # fx = 509.9508667
+    # fy = 509.9508667
+
+    cx = 207.4074707
+    cy = 204.50645447
+    fx = 318.7192688
+    fy = 318.7192688
+
+    L_paper = 150.0 ## mm
+
+
+        # fx/=1000
+        # fy/=1000
+    L_paper/=1000
+
+    horizontal_distance = 0.1 # move in 10 cm increments
+    yaw_delta = 15 # search for tag in increments of 15 degree
+    spun = False
+    with dai.Device(pipeline) as device:
+
+        print('Connected cameras:', device.getConnectedCameraFeatures())
+        # Print out usb speed
+        print('Usb speed:', device.getUsbSpeed().name)
+        # Bootloader version
+        if device.getBootloaderVersion() is not None:
+            print('Bootloader version:', device.getBootloaderVersion())
+        # Device name
+        print('Device name:', device.getDeviceName(), ' Product name:', device.getProductName())
+
+        # Output queue will be used to get the rgb frames from the output defined above
+        qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+
+        while True:
+            #inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
+            try:
+                #print('getting frame')
+                now = datetime.datetime.now()
+                print(now)
+                inRgb = qRgb.get().getCvFrame()
+                #color_img = np.asanyarray(inRgb)
+                # Retrieve 'bgr' (opencv format) frame
+                gray = cv2.cvtColor(inRgb, cv2.COLOR_BGR2GRAY) 
+                detections = detector.detect(gray, estimate_tag_pose=True, camera_params=[fx, fy, cx, cy], tag_size=L_paper)
+                #detections = False
+                if detections:
+                        # d = detections[0]
+                        # center = d['center']
+                        # cX = int(center[0])
+                        # cY = int(center[1])
+                        # dx, dy = get_depth_coords(cX, cY, rgb_width, rgb_height, depth_width, depth_height)
+                        # dist = depth_image[dy, dx]*depth_scale
+                        # offboard_control.set_distance_to_april(dist)
+                        # offboard_control.set_detection(True)
+                    d = detections[0]
+                    center = d.center
+                    center_x = int(center[0])
+                    center_y = int(center[1])
+
+                    pose_R = d.pose_R
+                        #yaw = np.degrees(np.arctan(pose_R[0][1]/pose_R[0][0]))
+                        #yaw = np.degrees(np.arctan(pose_R[1][2]/pose_R[2][2]))
+                    yaw = -np.degrees(np.arcsin(pose_R[0][2])) ## this the one chief]
+
+                    print('yawing to tag')
+
+                    if abs(yaw) > abs(yaw_delta):
+                        offboard_control.set_position_inst(0.0, 0.0, -np.sign(yaw)*yaw_delta) #just want to get drone lined up now
+                    else:
+                        offboard_control.set_position_inst(0.0, 0.0, -yaw)
+                    #print('yaw ,', yaw, ' degrees')
+                else:
+                    offboard_control.set_position_inst(0.0, 0.0, yaw_delta)
+                    print('yawing to find tag')
+                
+                
+                rclpy.spin_once(offboard_control)
+                spun = True
+
+            except Exception as e:     
+                print(e)           
+                offboard_control.destroy_node()
+                if spun:
+                    rclpy.shutdown()
+                sys.exit(0)
+                
+
+    ''' old, realsense version
     pipeline = rs.pipeline()
     config = rs.config()
 
@@ -394,7 +516,7 @@ def main(args=None) -> None:
             #     print('first detection spotted')
             #     offboard_control.arm()
             #     armed = True
-
+    '''
 
 
 
